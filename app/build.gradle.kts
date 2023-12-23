c305e485c84de9a2394a27c8e3e27674d4d8089b
hoangpm <hoangd16ptit@gmail.com>
2023-12-23 11:40:33 +0700

    Init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+import com.android.build.api.dsl.Packaging
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.com.google.dagger.hilt.android)
+}
+
+android {
+    namespace = "com.product.hstudio.simplepodomorotimer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.product.hstudio.simplepodomorotimer"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "0.0.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    packaging {
+        resources {
+            excludes.addAll(
+                listOf(
+                    "META-INF/gradle/incremental.annotation.processors"
+                )
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.bundles.navigation)
+    implementation(libs.bundles.viewmodel)
+    implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.hilt)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.datastore)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/product/hstudio/simplepodomorotimer/SimplePodomoroTimerApplication.kt
@@ -0,0 +1,7 @@
+package com.product.hstudio.simplepodomorotimer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SimplePodomoroTimerApplication: Application()
